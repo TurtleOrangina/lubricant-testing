3163cf408eb8505a552ede158c784dcadd306d68
MyNickName <mynickname@notvalidemail.com>
2026-05-14 17:27:39 +0200

    Don't use single file when building, keep output file names stable

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
 import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 750,
+    rollupOptions: {
+      output: {
+        codeSplitting: false,
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
   staged: {
     "*": "vp check --fix",
   },
   fmt: { ignorePatterns: ["docs/**"] },
   lint: { options: { typeAware: true, typeCheck: true } },
-  plugins: [vue(), viteSingleFile()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
